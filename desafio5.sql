0da3d2c0bf3f96fd0f1941b8a6f10b7374b93908
Bruno <brunocmoraes97@gmail.com>
2021-08-03 19:11:41 -0300

    Fix desafio 5. Estava faltando inserir casas decimais na varição salarial

--- desafio5.sql
+++ desafio5.sql
@@ -1,6 +1,6 @@
 SELECT
 JOB_title AS Cargo,
-ROUND((MAX_SALARY - MIN_SALARY), 2) AS 'Variação Salarial',
+CAST(MAX_SALARY- MIN_SALARY AS DECIMAL(16,2)) AS 'Variação Salarial',
 ROUND(MIN_SALARY/12, 2) AS 'Média mínima mensal',
 ROUND(MAX_SALARY/12, 2) AS 'Média máxima mensal'
 FROM hr.jobs
