330f04585ea122b7901b377c0432396a71e94c4e
Bruno <brunocmoraes97@gmail.com>
2021-08-03 19:16:29 -0300

    Fix desafio 6 e 5. String errada

--- desafio5.sql
+++ desafio5.sql
@@ -1,6 +1,6 @@
 SELECT
 JOB_title AS Cargo,
-CAST(MAX_SALARY- MIN_SALARY AS DECIMAL(16,2)) AS 'Variação Salarial',
+MAX_SALARY- MIN_SALARY AS 'Variação Salarial',
 ROUND(MIN_SALARY/12, 2) AS 'Média mínima mensal',
 ROUND(MAX_SALARY/12, 2) AS 'Média máxima mensal'
 FROM hr.jobs
--- desafio6.sql
+++ desafio6.sql
@@ -1,5 +1,5 @@
 SELECT
-CONCAT(tableEmployees.FIRST_NAME, ' ', tableEmployees.LAST_NAME) AS 'Nome Completo',
+CONCAT(tableEmployees.FIRST_NAME, ' ', tableEmployees.LAST_NAME) AS 'Nome completo',
 tableJobs.JOB_TITLE AS 'Cargo',
 tableHistory.START_DATE AS 'Data de início do cargo',
 tableDepartments.DEPARTMENT_NAME AS 'Departamento'
@@ -8,4 +8,4 @@ FROM hr.employees AS tableEmployees
 INNER JOIN job_history AS tableHistory ON tableHistory.EMPLOYEE_ID = tableEmployees.EMPLOYEE_ID
 INNER JOIN jobs AS tableJobs ON tableJobs.JOB_ID = tableHistory.JOB_ID
 INNER JOIN departments AS tableDepartments ON tableDepartments.DEPARTMENT_ID = tableHistory.DEPARTMENT_ID
-ORDER BY `Nome Completo` DESC, Cargo ASC;
+ORDER BY `Nome completo` DESC, Cargo ASC;
