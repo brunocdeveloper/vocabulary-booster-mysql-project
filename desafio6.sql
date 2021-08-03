a6c41c9e14f6cfe341b472c3915fbc67587beaab
Bruno <brunocmoraes97@gmail.com>
2021-08-03 18:20:48 -0300

    Fix desafio 6, a seleção estava com as tabelas trocadas, e isso puxava o departamento e o cargo antigo

--- desafio6.sql
+++ desafio6.sql
@@ -5,7 +5,7 @@ tableHistory.START_DATE AS 'Data de início do cargo',
 tableDepartments.DEPARTMENT_NAME AS 'Departamento'
 
 FROM hr.employees AS tableEmployees
-INNER JOIN jobs AS tableJobs ON tableJobs.JOB_ID = tableEmployees.JOB_ID
 INNER JOIN job_history AS tableHistory ON tableHistory.EMPLOYEE_ID = tableEmployees.EMPLOYEE_ID
-INNER JOIN departments AS tableDepartments ON tableDepartments.DEPARTMENT_ID = tableEmployees.DEPARTMENT_ID
+INNER JOIN jobs AS tableJobs ON tableJobs.JOB_ID = tableHistory.JOB_ID
+INNER JOIN departments AS tableDepartments ON tableDepartments.DEPARTMENT_ID = tableHistory.DEPARTMENT_ID
 ORDER BY `Nome Completo` DESC, Cargo ASC;
