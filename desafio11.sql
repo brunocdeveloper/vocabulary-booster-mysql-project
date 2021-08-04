2c5a2761ae0ec33642f4b5f4175e23b183d0ab7d
Bruno <brunocmoraes97@gmail.com>
2021-08-04 12:21:12 -0300

    Fix requisito 11. Faltou ordenar

--- desafio11.sql
+++ desafio11.sql
@@ -9,4 +9,5 @@ w3schools.customers AS t2
 WHERE t2.Country = t1.Country
 ) AS `Número de compatriotas`
 FROM w3schools.customers AS t1
-HAVING `Número de compatriotas` <> 0;
+HAVING `Número de compatriotas` <> 0
+ORDER BY `Nome`;
