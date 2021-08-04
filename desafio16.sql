b6ea77ff6993bd74ddac09b867634308d20f34ad
Bruno <brunocmoraes97@gmail.com>
2021-08-04 13:49:59 -0300

    Correção code climate

--- desafio16.sql
+++ desafio16.sql
@@ -4,15 +4,15 @@ DELIMITER $$
 CREATE FUNCTION buscar_quantidade_de_empregos_por_funcionario(email VARCHAR(50))
 RETURNS INT READS SQL DATA
 BEGIN
-	DECLARE contagem INT;
-	SELECT
-    COUNT(*)
-    FROM hr.job_history AS tableJobHistory
-    INNER JOIN hr.employees AS tableEmployees
-    ON tableEmployees.EMPLOYEE_ID = tableJobHistory.EMPLOYEE_ID
-    AND tableEmployees.EMAIL = email
-    INTO contagem;
-    RETURN contagem;
+DECLARE contagem INT;
+SELECT
+COUNT(*)
+FROM hr.job_history AS tableJobHistory
+INNER JOIN hr.employees AS tableEmployees
+ON tableEmployees.EMPLOYEE_ID = tableJobHistory.EMPLOYEE_ID
+AND tableEmployees.EMAIL = email
+INTO contagem;
+RETURN contagem;
 END $$
 
 DELIMITER $$
