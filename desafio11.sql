4e96cee2c84e47902376d26c87f3501d836fc07f
Bruno <brunocmoraes97@gmail.com>
2021-08-04 11:53:10 -0300

    Desafio 11

---
+++ desafio11.sql
@@ -0,0 +1,14 @@
+SELECT
+t1.ContactName AS 'Nome',
+t1.Country AS 'País',
+(
+SELECT
+COUNT(*) -1
+FROM
+w3schools.customers AS t2
+WHERE t2.Country = t1.Country
+) AS `Número de compatriotas`
+FROM w3schools.customers AS t1
+HAVING `Número de compatriotas` <> 0
+
+
