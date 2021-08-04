0463c77781f97556ae16413ea0cb0d4f0d7dc2f9
Bruno <brunocmoraes97@gmail.com>
2021-08-04 12:39:26 -0300

    Desafio 13

---
+++ desafio13.sql
@@ -0,0 +1,8 @@
+SELECT
+ProductName AS 'Produto',
+Price AS 'Price'
+FROM w3schools.products AS tableProducts,
+w3schools.order_details AS tableOrderDetails
+WHERE tableOrderDetails.ProductID = tableProducts.ProductID
+AND tableOrderDetails.Quantity > 80
+ORDER BY `Produto`;
