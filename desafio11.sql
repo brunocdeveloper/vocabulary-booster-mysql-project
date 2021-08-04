283ef1ab30ae6038408255d8a1f2cedce3106819
Bruno <brunocmoraes97@gmail.com>
2021-08-04 11:53:59 -0300

    Fix code climate

--- desafio11.sql
+++ desafio11.sql
@@ -9,6 +9,4 @@ w3schools.customers AS t2
 WHERE t2.Country = t1.Country
 ) AS `Número de compatriotas`
 FROM w3schools.customers AS t1
-HAVING `Número de compatriotas` <> 0
-
-
+HAVING `Número de compatriotas` <> 0;
