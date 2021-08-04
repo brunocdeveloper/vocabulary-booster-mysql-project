b2f5634201761da36e2d3c7ca764e079d7dc9ad2
Bruno <brunocmoraes97@gmail.com>
2021-08-04 13:01:05 -0300

    Correção desafio 14

--- desafio14.sql
+++ desafio14.sql
@@ -1,8 +1,7 @@
-SELECT
-tableCustomers.Country AS 'País'
-FROM w3schools.customers as tableCustomers
-INNER JOIN w3schools.suppliers AS tableSuppliers
-ON tableSuppliers.Country <> tableCustomers.Country
-GROUP BY `País`
+(SELECT Country AS 'País'
+FROM w3schools.customers)
+UNION
+(SELECT Country AS 'País'
+FROM w3schools.suppliers)
 ORDER BY `País`
 LIMIT 5;
